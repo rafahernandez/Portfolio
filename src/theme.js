ac720655643ead040ff000275c4a5c3bbfadb6f8
Rafael  Hernandez <joraheta@gmail.com>
2018-12-12 20:46:29 -0600

    Initial commit
    Initial commit

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+const colors = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6', //Gris fondo
+
+  primary: '#05445C',         //Links
+  primaryLight: '#08688c',
+  primaryDark: '#02202c',
+
+  secondary: '#5C1D05',
+  secondaryLight: '#8c2c08',
+  secondaryDark: '#2c0e02',
+};
+
+module.exports = {
+  colors,
+};
